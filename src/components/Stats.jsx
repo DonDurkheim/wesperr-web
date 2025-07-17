adc8212b2290f5ef30962f7924b569b48dfb455a
don_durkheim <don_durk@don.durkheim>
2025-07-17 16:41:18 +0200

    Hide stats numbers in mobile view

--- src/components/Stats.jsx
+++ src/components/Stats.jsx
@@ -17,7 +17,7 @@ const Stats = () => {
   };
 
   return (
-    <section className={`${styles.flexCenter} flex-row flex-wrap sm:mb-20 mb-6`}>
+    <section className={`${styles.flexCenter} flex-row flex-wrap sm:mb-20 mb-6 hidden sm:flex`}>
       {stats.map((stat) => (
         <motion.div
           key={stat.id}
