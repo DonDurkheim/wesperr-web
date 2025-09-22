fcfc1018155db0ded7eaa8efd4e653c7ad3afc7c
Don Durkheim <dondurkheim13@gmail.com>
2025-09-22 21:11:27 +0200

    mongodb atlas configured

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 const { VITE_API_BASE_URL, PROD } = import.meta.env;
-const baseURL = VITE_API_BASE_URL || "https://wesperr-web-backend.onrender.com";
+const baseURL = VITE_API_BASE_URL;
 
 const api = axios.create({ baseURL });
 
